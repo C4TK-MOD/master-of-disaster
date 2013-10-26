58bff46fb1ab953de9d284f9009005689601287b
Justin McDaniel <jmcdaniel001@gmail.com>
2013-10-26 03:05:12 -0500

    Deleted incorrect omniauth line.

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,5 @@ module Newapp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    OmniAuth.config.logger = Rails.logger
   end
 end
