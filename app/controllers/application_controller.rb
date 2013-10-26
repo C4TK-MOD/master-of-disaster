d86308276c3b9880f30133c828f261e32ec55102
Justin McDaniel <jmcdaniel001@gmail.com>
2013-10-26 13:55:16 -0500

    Forgot to add auth filter to base controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :authenticate
+
   def authenticate
     redirect_to login_path unless session && session.has_key?(:user_id)
   end
