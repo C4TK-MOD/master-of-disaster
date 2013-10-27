1c347856c6fa6d946e4a5770dfbf85fde14cc829
Jonathan Hardy <jhardy@peopleadmin.com>
2013-10-26 21:06:24 -0500

    Adding default data (not yet complete).

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131026214129) do
+ActiveRecord::Schema.define(:version => 20131026233810) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
